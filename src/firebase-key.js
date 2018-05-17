2a3e720f787932c1f12b1e30fc4f5d3259c19df8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-17 16:28:25 +0000

    Deploy powerc9000/tablechamp-app to github.com/powerc9000/tablechamp-app.git:gh-pages

--- src/firebase-key.js
+++
@@ -1,7 +0,0 @@
-const data = { 
-"apiKey": 'AIzaSyCzqfIyWs18CxfAcO-p71MZ6sf77AmMaP0', 
-  "authDomain": 'table-champ-fc7a1.firebaseapp.com', 
-  "databaseURL": 'https://table-champ-fc7a1.firebaseio.com' 
-};
-
-export default data;
